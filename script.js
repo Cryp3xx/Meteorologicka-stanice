b47740e8d688c37878d3189ded67ec40bbe5a86c
Cryp3xx <145440436+Cryp3xx@users.noreply.github.com>
2024-04-23 13:25:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let Ce = "Polojasno";
+let Sl = "Oblačno";
+let Ne = "Polojasno";
+let Sv = "Oblačno";
+let It = "Oblačno";
+
+
+function PocasiCesko() {
+    document.getElementById("resultpocasi").textContent = Ce;
+}
+
+function PocasiSlovensko() {
+    document.getElementById("resultpocasi").textContent = Sl;
+}
+
+function PocasiNemecko() {
+    document.getElementById("resultpocasi").textContent = Ne;
+}
+
+function PocasiSvycarsko() {
+    document.getElementById("resultpocasi").textContent = Sv;
+}
+
+function PocasiItalie() {
+    document.getElementById("resultpocasi").textContent = It;
+}
